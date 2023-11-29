ccd395aaf60846bce235faf0dff910c459bc2da8
Pascual <pascual2@llnl.gov>
2023-11-29 08:49:25 -0800

    Unexpected TreeTable header behavior (stickyHeader=true and embedded iside Splitter)

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,16 +1,17 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
 import lombok.Data;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
 
 @Data
 @Named
@@ -23,6 +24,8 @@ public class TestView implements Serializable {
     private LocalDateTime localDateTime;
     private List<TestObject> list;
     
+    private TreeNode<TestObject> root;
+    
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
@@ -32,6 +35,11 @@ public class TestView implements Serializable {
                 new TestObject("The Bodyguard", "Whitney Houston", 1992),
                 new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
         ));
+        
+        root = new DefaultTreeNode<>(new TestObject("", "", 0), null);
+        list.stream().forEach(to -> new DefaultTreeNode<>(to, root));
+        
+        
     }
 
 }
